4a997a721d507428b60966d2bd0f2fd83410f4da
coderRohit620 <rohitravi62023@gmail.com>
2026-05-10 00:43:42 +0530

    fix: update CORS configuration to include methods and default origin

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,12 @@
 import express from "express";
-import cors from "cors"
+import cors from "cors";
 import cookieParser from "cookie-parser";
 
 const app = express();
 app.use(cors({
-    origin:process.env.CORS_ORIGIN,
-    credentials: true
+    origin:process.env.CORS_ORIGIN || "http://localhost:5173",
+    methods: ["GET", "POST", "PATCH", "DELETE"],
+    credentials: true,
 }))
 
 app.use(express.json({limit:"16kb"}))
